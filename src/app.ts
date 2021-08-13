9d15a4f9b82582d0e741040c0f68ebf5d2fd953b
Kavtorev <dmitrijkavtorev@gmail.com>
2021-08-13 14:16:34 +0200

    added express initializer

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { NextFunction, Request, Response } from "express";
+
+function initializeApplication() {
+  const app = express();
+
+  app.use(express.json());
+
+  app.use((req, res) => {
+    res.status(404).json({ message: "Not Found" });
+  });
+
+  // global error handler.
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status || 500).json({
+      message: err.message || "Internal server error",
+      extra: err.extra,
+    });
+  });
+
+  return app;
+}
+
+export { initializeApplication };
